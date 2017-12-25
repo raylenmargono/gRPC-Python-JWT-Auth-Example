77c4b3e4fc1630c7a293c859658da23922a08962
Raylen Margono <raylenmargono@Raylens-MBP.charter.com>
2017-12-25 10:16:00 -0800

    README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Demo:
+
+1. docker-compose up
+2. use AuthenticationClient to create/login user and retrieve token
+3. use ACCESS_TOKEN and token in step 2. to access kensho_service
+   using KenshoClient
--- run.py
+++
@@ -1,8 +0,0 @@
-from user_service.client import AuthenticationClient
-from kensho_service.client import KenshoClient
-
-c = AuthenticationClient('localhost:8000')
-
-token = c.login('test2@test.com', 'test').get('token')
-k = KenshoClient('localhost:5000', token, 'access_token')
-k.do_admin_kensho()
